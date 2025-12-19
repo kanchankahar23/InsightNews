2e615e24f5d66bac6107792758ed0ba5adfd7cd3
kanchan kahar <kaharkanchan884@gmail.com>
2025-12-19 20:59:58 +0530

    added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
 const Home = () => {
+  const apikey = "30cefa94e9f44bfca529801ac57cd45e"
+  // https://newsdata.io/api/1/latest?apikey=pub_8c6da57f55e84c6d82eece4f5d45fb34&q=sports;
+
   return (
     <div>
-        sbbskbsckj
+      <div>
+
+      </div>
     </div>
   )
 }
