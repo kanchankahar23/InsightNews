04bc3b4e414e068c60b0890954cedb56e983844d
kanchan kahar <kaharkanchan884@gmail.com>
2025-12-19 11:20:43 +0530

    added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,28 @@
 import React from 'react'
+import image from '../assets/image copy.png'
 
-import image from '../assets/Logo.png'
 const Navbar = () => {
   return (
-    <div className='w-full h-20 bg-amber-700 flex justify-center items-center gap-16'>
+    <div className='w-full h-28 bg-zinc-50 flex justify-between items-center px-10 shadow-md'>
+
       <div>
-        <img className='w-30 h-30' src={image} alt="" />
+        <img className='w-44 h-44 object-contain' src={image} alt="Logo" />
       </div>
-      <nav>
-        <a href="">Home</a>
-        <a href="">Technology</a>
-        <a href="">Sport</a>
-        <a href="">Business</a>
-        <a href="">Entertainment</a>
-        <a href="">Health</a>
+
+      {/* Navigation Links */}
+      <nav className='flex items-center gap-8 text-gray-700 font-medium'>
+        <a href="#" className='hover:text-blue-800 font-bold text-xl  transition'>HOME</a>
+        <a href="#" className='hover:text-blue-800 font-bold text-xl  transition'>TECHNOLOGY</a>
+        <a href="#" className='hover:text-blue-800 font-bold text-xl  transition'>SPORTS</a>
+        <a href="#" className='hover:text-blue-800 font-bold text-xl  transition'>BUSINESS</a>
+        <a href="#" className='hover:text-blue-800 font-bold text-xl  transition'>ENTERTAINMENT</a>
+        <a href="#" className='hover:text-blue-800 font-bold text-xl  transition'>HEALTH</a>
       </nav>
-      <button>Get Started</button>
+
+      {/* Button */}
+      <button className='bg-blue-600 text-white px-6 py-2 rounded-md hover:bg-blue-700 transition'>
+        Get Started
+      </button>
     </div>
   )
 }
