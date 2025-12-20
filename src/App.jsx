6156bc3b3c4dd8cb9740ef84cfc83f46678da16e
kanchan kahar <kaharkanchan884@gmail.com>
2025-12-20 23:11:13 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Middle from './components/Middle'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
      <Navbar/>
      <Home/>
      <Middle/>
+     <Footer/>
     </>
   )
 }
